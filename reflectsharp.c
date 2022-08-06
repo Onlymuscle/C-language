488e1620fa5f665b3c14fa4a6bfc7005e45a9d48
Onlymuscle <108688212+Onlymuscle@users.noreply.github.com>
2022-08-06 19:46:52 +0900

    ReflectSharp
    It's been a long time The reason for the lack of activity is not only studying C language, but also studying various things, and these days, I am being treated for injuries, and I am working separately, so I did not have enough time. Let's hang in there for a month.
    And this is the ReflectSharp task in the book. I don't know how it works because I haven't found an example. I tried to make it as much as I could.

---
+++ reflectsharp.c
@@ -0,0 +1,61 @@
+﻿#include<stdio.h>
+#include<windows.h>
+#include<conio.h>
+
+void gotoxy(int x, int y)
+{
+	COORD pos = { x,y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+void main()
+{
+	int x = 1, y = 1, i;
+
+	system("cls");
+
+	for (;;) {
+		if (_kbhit()) {
+			i = _getch();
+			Sleep(20);
+			system("cls");
+
+			switch (i) {
+			case '7':
+				gotoxy(x--, y--);
+				putchar('#');
+				break;
+			case '1':
+				gotoxy(x--, y++);
+				putchar('#');
+				break;
+			case '3':
+				gotoxy(x++, y++);
+				putchar('#');
+				break;
+			case '9':
+				gotoxy(x++, y--);
+				putchar('#');
+				break;
+			case '-':
+				exit(1);
+				break;
+			}
+			if (x == 50) {
+				x = x - 2, y--;
+				putchar('#');
+			}
+			else if (y == 25) {
+				x--, y = y - 2;
+				putchar('#');
+			}
+			else if (x == 0) {
+				x = x + 2, y--;
+				putchar('#');
+			}
+			else if (y == 0) {
+				x--, y = y + 2;
+				putchar('#');
+			}
+		}
+	}
+}
